ea59c4f2d5d14c71a98e98d5b7bd9efa29c83fcb
Apple <apple@Apples-MacBook-Pro.local>
2019-07-08 14:54:31 -0700

    First save

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var str = "Hello, playground"
+var age = 18
+"I am \(age) years old"
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1)(string2)"
+ ("Hello, World")
+
+var first = "Karlie"
+var last = "Kloss"
+
+
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+(a+b+c+d)
+Double(a+b+c+d)/4
+
+var name = "Allysson Lingad"
+
+var address = "1234 S London"
+"I am \(name) and I am \(age) years old"
+
+var birthday = Double(age+1)
+
+5 < 3
+12 > 7
+6 != 8
+"karlie" == "karlie"
+"karlie" == " karlie "
+
+var dogAge = 1
+if dogAge < 2 {
+    print ("You are a puppy!")
+    
+}
---
+++ day1 pt2.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+
+var dogAge = 1
+if dogAge < 2 {
+    print ("You are a puppy!")
+}
+
+else if dogAge > 12 {
+    print("You are elderly")
+}
+
+else {
+    print("You are also awesome")
+}
+
+var favFood = "Chipotle"
+if favFood == "Chipotle"{
+    print ("YAY SAME")
+}
+else if favFood == "Starbucks"{
+    print ("that's okay too!")
+}
+
+var hasAccount = true
+if hasAccount == false{
+    print ("Please log in")
+}
+
+else if hasAccount == true{
+    print ("Here's your account")
+}
+
+var gradeLvl = 12
+if gradeLvl == 12{
+    print ("You still have to finish high school!")
+}
